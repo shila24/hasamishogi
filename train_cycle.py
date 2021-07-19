291f62a8ba5a21ac7956bcbf368aa02a139ed8cb
白倉達啓 <shirakuratatsuhiro@shirakuratoorukeinoMacBook-Air.local>
2021-07-19 16:47:53 +0900

    first commit

---
+++ train_cycle.py
@@ -0,0 +1,20 @@
+# パッケージのインポート
+from dual_network import dual_network
+from self_play import self_play
+from train_network import train_network
+from evaluate_network import evaluate_network
+
+
+# デュアルネットワークの作成
+dual_network()
+
+for i in range(10):
+    print('Train',i,'====================')
+    # セルフプレイ部
+    self_play()
+
+    # パラメータ更新部
+    train_network()
+
+    # 新パラメータ評価部
+    evaluate_network()
